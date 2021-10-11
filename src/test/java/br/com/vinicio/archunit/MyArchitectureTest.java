e91ec20aa3895d0cb4811a7035b5a74e917f5cb9
vinicio <vinicioferreira@sonner.com.br>
2021-10-11 08:13:48 -0300

    second commit

--- src/test/java/br/com/vinicio/archunit/MyArchitectureTest.java
+++ src/test/java/br/com/vinicio/archunit/MyArchitectureTest.java
@@ -3,19 +3,48 @@ package br.com.vinicio.archunit;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 
 public class MyArchitectureTest {
 
     @Test
-    public void some_architecture_rule() {
+    public void services_should_only_be_accessed_by_resources_and_services() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("br.com.vinicio.archunit");
         ArchRule myRule = classes()
                 .that().resideInAPackage("..service..")
-                .should().onlyBeAccessed().byAnyPackage("..controller..", "..service..");
+//                .that().areAnnotatedWith(Service.class)
+                .should().onlyBeAccessed().byAnyPackage("..resource..", "..service..");
         myRule.check(importedClasses);
+    }
+
+    @Test
+    public void repositories_should_only_be_accessed_by_services() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("br.com.vinicio.archunit");
+        ArchRule myRule = classes()
+                .that().resideInAPackage("..repository..")
+                .should().onlyBeAccessed().byAnyPackage("..service..");
+        myRule.check(importedClasses);
+    }
+
+    @Test
+    public void should_not_violate_layers() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("br.com.vinicio.archunit");
+
+        Architectures.LayeredArchitecture arch = layeredArchitecture()
+                // Define layers
+                .layer("Resource").definedBy("..resource..")
+                .layer("Service").definedBy("..service..")
+                .layer("Repository").definedBy("..repository..")
+                // Add constraints
+                .whereLayer("Resource").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Resource", "Service")
+                .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
+        arch.check(importedClasses);
+
     }
 }
